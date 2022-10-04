02bb05deffc8eba4d695c666cc2c9e974b48957f
Craig <cragt369@gmail.com>
2022-10-04 16:50:51 -0500

    Add color and reset buttons, size input, draw function

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-const grid = document.getElementById("container");
+let color = "black";
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (i = 0; i < 9; i++) {
-  const div = document.createElement("div");
-  div.id = "pixel";
-  grid.appendChild(div);
-  // const divBoxStyle = document.getElementById('pixel');
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+  if ((input >= 2) & (input <= 100)) {
+    populateBoard(input);
+  } else {
+    console.log("Too many squares");
+  }
+}
+
+function colorSquare() {
+  if (color === "Random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function getRandomColor(choice) {
+  var letters = "0123456789ABCDEF";
+  var randomColor = "#";
+  for (var i = 0; i < 6; i++) {
+    randomColor += letters[Math.floor(Math.random() * 16)];
+  }
+  color = randomColor;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "white"));
 }
