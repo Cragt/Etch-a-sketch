cca8f5dc4aa66042f10522cf01ccc77c2c54c4a0
Craig <cragt369@gmail.com>
2022-10-06 15:32:39 -0500

    Add CSS styling, center elements, add size input error messages

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function populateBoard(size) {
   squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+  document.getElementById("title").innerHTML = "SketchPad";
 
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
@@ -20,8 +21,12 @@ populateBoard(16);
 function changeSize(input) {
   if ((input >= 2) & (input <= 100)) {
     populateBoard(input);
-  } else {
-    console.log("Too many squares");
+  } else if (input > 100) {
+    document.getElementById("title").innerHTML = "Too many squares!";
+  } else if (input < 2) {
+    document.getElementById("title").innerHTML = "Too few squares!";
+  } else if (input != Number) {
+    document.getElementById("title").innerHTML = "Please enter a number";
   }
 }
 
